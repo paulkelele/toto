ea4a5377c6d94e5ac42b7dc698c0df1a38a8cddc
stephane boyer <stephane.boyer@wraptor.fr>
2022-11-22 17:20:17 +0100

    clean code

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, HostBinding, Input} from '@angular/core';
+import { Component, ViewChild, ElementRef, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatTableDataSource } from "@angular/material/table";
 import { materialModules } from './material';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 import { TableComponent } from './table.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HeaderComponent } from './header.component';
@@ -40,7 +37,7 @@ declare const window: any;
 }`
   ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent   {
 
   @HostBinding('class') className = '';
   @ViewChild('in')  input: ElementRef | undefined;
@@ -57,40 +54,16 @@ export class LoginComponent implements OnInit {
   fileData:any;
   numberRows:number |undefined ;
 
-  // @ViewChild(MatPaginator, {static: false})
-  // set paginator(value: MatPaginator | undefined) {
-  //   this.dataSource.paginator = value;
-  // }
-  // @ViewChild(MatSort, {static: false}) 
-  // set sort(value: MatSort | undefined) { 
-  //   this.dataSource.sort = value
-  // };
-
   constructor(private _snackBar: MatSnackBar) {
-   
   }
 
-  ngOnInit(): void {
-    // this.toggleControl.valueChanges.subscribe((darkMode) => {
-    //   console.log("Je suis appelé");
-    //   const darkClassName = 'darkMode';
-    //   this.className = darkMode ? darkClassName : '';
-    // });
-   
-   }
-   
-
-sendFormContolAction($event: FormControl) {
-  $event.valueChanges.subscribe((darkMode) => {
-  console.log("Je suis appelé");
-  const darkClassName = 'darkMode';
-  this.className = darkMode ? darkClassName : '';
-});
+  receiveToggleControlForDarkMode($event: FormControl):void {
+    let val:boolean = $event.value
+    val ? this.className='darkMode' : this.className='';
 }
   
   async load(event: any) {
   this.byLineArray = [];
-  console.log(event);
     const file:File = event.target.files[0];
     if(file){
       if (file.size === 0){
@@ -107,7 +80,6 @@ sendFormContolAction($event: FormControl) {
 }
  
 removeTab(index: number) {
-     
     this.tabs2[0].splice(index, 1);
     this.tabs2[1].splice(index, 1);
     if(this.input)
